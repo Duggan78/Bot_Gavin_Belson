a910a4d2c040a24650d359fc3d45c8a9da961852
Jack Duggan <53382645+Duggan78@users.noreply.github.com>
2020-05-23 12:29:16 -0400

    working on commands

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import discord
+from discord.ext import commands
 
 client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
 
 # Going to add the main commands down here.
-@client.commands
+@bot.command()
+async def test(ctx):
+    await ctx.send('testing passed!')
 
 
-client.run()
+client.run('')
