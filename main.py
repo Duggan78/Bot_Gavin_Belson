7e8430f00b754286ae1b6c021c78e1b5bcdd27ff
Jack Duggan <53382645+Duggan78@users.noreply.github.com>
2020-05-23 16:09:50 -0400

    added new commands and other edits
    !ping !shutdown !clear !invitelink all have been added to the bot

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import discord
+import discord.client
 from discord.ext import commands
+import asyncio
 
-client = discord.Client()
-bot = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='!')
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-
 # Going to add the main commands down here.
-@bot.command()
-async def test(ctx):
-    await ctx.send('testing passed!')
+@client.command()
+async def shutdown(ctx):
+    await client.close()
+    print("the client has disconnected itself")
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+
+@client.command()
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount)
+# Clear command stolen from github.com/baconman322... Thank You! haha
+
+@client.command()
+async def invitelink(ctx):
+    await ctx.send("https://discord.gg/cvWBwj")
 
 
 client.run('')
